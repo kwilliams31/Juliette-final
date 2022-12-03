5121f2ff4d0bd86a46b9fba01311e3be8d62b726
Kristina <111464160+kwilliams31@users.noreply.github.com>
2022-12-03 13:37:16 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,36 @@
-//
+$(document).ready(function() { 
+
+
+    $(function () { 
+        $.each(welcomeInfoArray, (index, value) => {
+        console.log('<div class="card col-md-4"> ' +
+        value.hello +
+        '<div class="card-body">' +
+        value.reasonOne +'"</div></div>'
+    );
+
+$('#welcome').append(
+
+    `
+    <div id="welcomeCard" class="card col-sm-6 offset-sm-3">
+    <div class="card-body">
+      <h5 class="card-title">${ welcomeInfoArray[index].hello }</h5>
+      <p class="card-title">${ welcomeInfoArray[index].reasonOne }</p>
+
+    </div>
+  </div>
+    `
+  )
+
+});
+});
+});
+
+// card info for introduction to page
+let welcomeInfoArray = [
+    {
+    hello: "Hello! Welcome to the final project of CIS 376 (aka the favorite class)",
+    reasonOne: "Echo Game", 
+    }
+  
+  ]
