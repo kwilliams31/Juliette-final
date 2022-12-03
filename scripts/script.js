8d0b46d1f4f0a275f185e5b61dba5168b96ba234
Kristina <111464160+kwilliams31@users.noreply.github.com>
2022-12-03 17:52:29 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function() { 
 
+    // event listener to make the buttons looked pressed down when clicked on
+    const buttons = document.querySelectorAll('button');
 
+    buttons.forEach(button => {
+    button.addEventListener('click', () => {
+    buttons.forEach(button => button.classList.remove('active'));
+    button.classList.add('active');
+    });
+});
+
+// function for welcome card
     $(function () { 
         $.each(welcomeInfoArray, (index, value) => {
         console.log('<div class="card col-md-4"> ' +
@@ -9,6 +19,8 @@ $(document).ready(function() {
         value.reasonOne +'"</div></div>'
     );
 
+
+// appending the card and info on the card in html
 $('#welcome').append(
 
     `
@@ -26,11 +38,32 @@ $('#welcome').append(
 });
 });
 
+
 // card info for introduction to page
 let welcomeInfoArray = [
     {
-    hello: "Hello! Welcome to the final project of CIS 376 (aka the favorite class)",
-    reasonOne: "Echo Game", 
+    hello: "Hello! Welcome to the final project of CIS 376",
+    reasonOne: "UX design should be important for interface. I have given an example of a bad UX as well as an improved version of how I think things could work better.", 
     }
   
   ]
+
+  // functions for buttons of bad and good UX hidden before clicked on and shows after click
+  function myFunction1() {
+    var cardDisplay = document.getElementById("badUX");
+    if (cardDisplay.style.display === "none") {
+        cardDisplay.style.display = "block";
+    } else {
+      cardDisplay.style.display = "none";
+    }
+  }
+
+  // for good UX to show when clicked
+  function myFunction2() {
+    var cardDisplay = document.getElementById("goodUX");
+    if (cardDisplay.style.display === "none") {
+        cardDisplay.style.display = "block";
+    } else {
+      cardDisplay.style.display = "none";
+    }
+  }
